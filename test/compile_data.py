415a073d8e2c21950b99647805132be19024ad25
Jonathan Braden <jonathan.n.braden@gmail.com>
2020-10-01 11:35:30 -0400

    Sorting program into sections

--- test/compile_data.py
+++ test/compile_data.py
@@ -4,16 +4,18 @@ import json
 import os
 
 template_dir = 'templates/'
+slide_dir = '../Data/'
+data_dir = 'Data/'
 
-sections = [
-    { 'title' : 'Introduction',    'people' : [] },
-    { 'title' : 'Radio Astronomy', 'people' : ['Ue-Li Pen' ] },
-    { 'title' : 'Cosmology',       'people' : ['Dick Bond', 'Jonathan Braden', 'Thomas Morrison', 'Pavel Motloch', 'Alex Lagu\u00eb', 'Nathan Carlson', 'Martine Lokken'] },
-    { 'title' : 'General Astro',   'people' : [] },
-    { 'title' : 'Other',           'people' : [] }
+sec_data = [
+    { 'title' : 'Introduction',    'people' : [], 'time' : '15:00-15:20' },
+    { 'title' : 'Early Universe, Cosmology, and Galaxies', 'time' : '15:20-15:35', 'people' : [u'Alex Lagu\xeb', 'Dongwoo Chung', 'Emily Tyhurst','James Willis','Jennifer Chan','Martine Lokken','Nathan Carlson','Pavel Motloch','Jonathan Braden'] },
+    { 'title' : 'Scintillometry, FRBs, and Pulsars', 'time' : '15:35-15:45', 'people' : ['Dylan Jow', 'Hsiu-Hsien Lin','Jonathan Zhang','Parasar Thulasiram','Ted Mackereth'] },
+    { 'title' : 'Stars, Compact Objects, and Planets', 'time' : '15:45-16:00', 'people' : ['Almog Yalinewich','Alysa Obertas','Eric Poisson','Janosz Dewberry','Norman Murray','Scott Tremaine','Wei Zhu','Chris Thompson','J. J. Zanazzi'] },
+    { 'title' : 'Misclassified', 'time' : 'unknown' }
     ]
 
-def read_presenter_data(dir='Data'):
+def read_presenter_data(dir):
     """
     Read the presenter data (stored as a bunch of individual JSON files),
     and return a list
@@ -23,19 +25,31 @@ def read_presenter_data(dir='Data'):
     Output:
       people - A list of dictionaries.  Each dictionary contains the JSON info for a single person
     """
-    files = glob(dir+'/*.json')
+    files = glob(dir+'*.json')
     people = []
     for fc in files:
         with open(fc) as f:
             people.append(json.load(f))
-    # Add in sorting into groups here
     return people
 
-def group_people(people):
-    # 1. Start by popping off current person
-    # 2. See if they are in each section
-    # 3. 
-    return
+def group_people(people,sections):
+    sec_list = [ [] for i in range(len(sections)) ]
+    for i in range(len(people)):
+        p_ = people.pop()
+        if p_['name'] in sections[1]['people']:
+            sec_list[1].append(p_)
+        elif p_['name'] in sections[2]['people']:
+            sec_list[2].append(p_)
+        elif p_['name'] in sections[3]['people']:
+            sec_list[3].append(p_)
+        else:
+            sec_list[4].append(p_)
+
+    if (len(sec_list[-1]) > 0):
+        print('Warning, missing presenters')
+        for p in sec_list[-1]:
+            print(p['name'])
+    return sec_list
 
 def compute_times(start,talk_len):
     """
@@ -56,19 +70,22 @@ def convert_time_to_string(min):
     """
     return '{:d}:{:02d}'.format(min//60,min%60) 
 
-def create_program(people):
+def create_program(people,sections=sec_data):
     """
     Write a LaTeX file with the program metadata.
     Stored in program.tex.
 
     Note: Any existing program.tex file is overwritten
     """
+    # Start by grouping people into sections
+    sec_list = group_people(people,sections)
 
     # These first two lines are horrendous code style.  Fix them.
     os.system('cat {}program_header.tex > program.tex'.format(template_dir))
     prog = open('program.tex','a')
     slides = [] # This will store the ordered list of slides
-    _make_section(prog,people)
+    for i,s_ in enumerate(sec_list):
+        _make_section(prog,s_,sections[i]['title'],sections[i]['time'])
     prog.write('\\end{document}\n')
     prog.close()
 
@@ -76,12 +93,12 @@ def create_program(people):
     #os.system('rm -f program.log program.aux')
     return
 
-def _make_section(prog,sec_people,title='Astrophysics'):
+def _make_section(prog,sec_people,title,time):
     """
     Input
       prog - The program tex file we're writing
     """
-    prog.write('\\textbf{\\huge %s}'%title)
+    prog.write('\\textbf{\\LARGE %s - %s}' % (title,time))
     prog.write('\\newline\n')
     prog.write('\\begin{center}\n')
     for p in sec_people:
@@ -95,7 +112,7 @@ def _write_program_entry(prog,info):
       prog - The program tex file we're writing
       info - JSON info for the person we're including  
     """
-    prog.write('\\begin{tabular}{|m{5cm}|m{10cm}|}\n')
+    prog.write('\\begin{tabular}{|m{5.5cm}|m{10.5cm}|}\n')
     prog.write('\\hline\n')
     tmpStr = '{\\bf '+info['name']+' } \\newline ' \
              +info['email'] \
@@ -106,10 +123,13 @@ def _write_program_entry(prog,info):
     prog.write('\\end{tabular}\n')
     return
 
-def create_slides():
+def create_slides(files):
+    """
+    Creates a compiled pdf of slides
+    """
     return
 
 if __name__=="__main__":
-    people = read_presenter_data()
+    people = read_presenter_data('Data/')
     create_program(people)
     pass
